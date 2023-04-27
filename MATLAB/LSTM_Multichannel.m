d54da3ab56b4e26044d4ea309a4a7c4e147f2629
Dawson J. Gullickson <dgullickson@mines.edu>
2023-04-26 20:26:19 -0600

    Added graphing capability to LSTM_Multichannel

--- MATLAB/LSTM_Multichannel.m
+++ MATLAB/LSTM_Multichannel.m
@@ -19,11 +19,7 @@ if ~exist("lstm_net", "var") || ~exist("l_seq", "var") || seq_to_load ~= l_seq
 
     % Split this sequence into individual training sequences
     train_seq_len = 40;
-
-    train_seqs = floor((length(seq) - 1) / train_seq_len);
-
-    %inputs = zeros(train_seqs, train_seq_len, 9);
-    %responses = zeros(train_seqs, train_seq_len, 9);
+    train_seqs = floorDiv(length(seq) - 1, train_seq_len);
 
     inputs = cell(train_seqs, 1);
     responses = cell(train_seqs, 1);
@@ -67,12 +63,13 @@ end
 sequenceLength = initializeSymbolMachine( ...
     sprintf('sequence_%s_test.mat', l_seq) ...
 );
+test_data = zeros(sequenceLength, 1);
+predicted_data = zeros(sequenceLength, 2);
 
 lstm_net = resetState(lstm_net);
 last_symbol = ones(9,1) ./ 9;
 
 for i = 1:sequenceLength
-
     [lstm_net, predicted] = predictAndUpdateState(lstm_net, last_symbol);
 
     % Make sure the prediciton is normalized, positive, and reasonable
@@ -83,11 +80,26 @@ for i = 1:sequenceLength
     last_symbol = zeros(9,1);
     last_symbol(actual_sym) = 1;
 
+    test_data(i) = actual_sym;
+    [~, sorted] = sort(predicted, "descend");
+    predicted_data(i, 1) = sorted(1);
+    predicted_data(i, 2) = sorted(2);
+
     % Print out a heartbeat every x steps
     if mod(i, 100) == 0
         disp("Completed " + i + " iterations");
     end
-
 end
 
-reportSymbolMachine
+reportSymbolMachine;
+
+% % Plot actual vs predicted
+% figure;
+% title("Actual vs Predicted");
+% xlabel("Time");
+% legend();
+% hold on;
+% plt1 = plot(test_data, 'DisplayName', 'Actual');
+% plt2 = plot(predicted_data(:, 1), "--", 'DisplayName', 'First Prediction');
+% plt3 = plot(predicted_data(:, 2), "--", 'DisplayName', 'Second Prediction');
+% hold off;
